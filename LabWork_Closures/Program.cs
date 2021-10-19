6f3355ed727e8f30d849c8d3b2375596a274caba
ab.smirnov <ab.smirnov@stalcom.com>
2021-10-19 12:22:40 +0300

    Добавлена демонстрация замыкания на примере делегатов со ссылочными и значимыми типами

---
+++ LabWork_Closures/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace LabWork_Closures
+{
+	internal class Program
+	{
+		#region Private
+		private static void Main(string[] args)
+		{
+			ClosureOnReferenceType();
+			Console.WriteLine();
+			ClosureOnSignificantType();
+			Console.ReadLine();
+		}
+
+		private static void ClosureOnReferenceType()
+		{
+			var str = "Initial value";
+
+			Action action = () =>
+			{
+				Console.WriteLine(str);
+				str = "Modified by closure";
+			};
+
+			str = "After delegate creation";
+			action();
+			Console.WriteLine(str);
+		}
+
+		private static void ClosureOnSignificantType()
+		{
+			var digit = 1;
+
+			Action action = () =>
+			{
+				Console.WriteLine(digit);
+				digit = 222;
+			};
+
+			digit = 3;
+			action();
+			Console.WriteLine(digit);
+		}
+		#endregion
+	}
+}
